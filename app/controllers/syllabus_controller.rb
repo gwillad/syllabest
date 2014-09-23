e78dc34853d9915998285f27ae998504a9d2ceb5
gwillad <adamgwilliam@gmail.com>
2014-09-23 15:54:07 -0400

    starting syllabus controller

--- app/controllers/syllabus_controller.rb
+++ app/controllers/syllabus_controller.rb
@@ -1,2 +1,35 @@
 class SyllabusController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @syllabus = Syllabus.new(params[:syllabus])
+
+    @syllabus.save
+    redirect_to @syllabus
+  end
+
+  def show
+    @syllabus = Syllabus.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def syllabus_params
+    params.require(:syllabus).permit(:title, :location, 
+                                     :course_num, :department, :term)
+  end
+    
+
+
+
 end
