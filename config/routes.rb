098f31e7574493a8535675113a862d622983f84c
Christopher Lepre <clepre@gemini.cs.hamilton.edu>
2014-09-23 17:31:50 -0400

    made users#index the root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Syllabest::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'users#index'
 
   resources :users
 
