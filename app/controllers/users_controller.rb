ff8e5293880b69429b2d91a9873e09d76063fce9
Christopher Lepre <clepre@gemini.cs.hamilton.edu>
2014-09-23 19:12:36 -0400

    changed @article to @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @article
+      redirect_to @user
     else
       render 'new'
     end
