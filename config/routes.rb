f2621ab46dd6d95f54cd89d1409b3a5c924bdcae
Christopher Lepre <clepre@gemini.cs.hamilton.edu>
2014-09-23 17:43:06 -0400

    added nested syllabus resource to user resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Syllabest::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
-  resources :users
+  resources :users do
+    resources :syllabuses
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
