5d7d2d5574d00a542ae9d908c1fcccb3d99eae8a
Christopher Lepre <clepre@gemini.cs.hamilton.edu>
2014-09-29 18:48:35 -0400

    changed type to component_type in params

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,5 +1,4 @@
 class ComponentsController < ApplicationController
-<<<<<<< HEAD
 	def new
 		@component = Component.new
 	end
@@ -24,35 +23,6 @@ class ComponentsController < ApplicationController
 
 	private
 		def component_params
-			params.require(:component).permit(:name, :type, :order, plaintext_attributes: [:title, :contents, :component_id])
+			params.require(:component).permit(:name, :component_type, :order, plaintext_attributes: [:title, :contents, :component_id])
 		end
-ends
-=======
-  def new
-    @component = Component.new
-  end
-  
-  def create
-    @component = Component.new(component_params)
-    
-    if @component.save
-      redirect_to @component
-    else
-      render 'new'
-    end
-  end
-  
-  def index
-    @components = Component.all
-  end
-  
-  def show
-    @component = Component.find(params[:id])
-  end
-  
-  private
-  def component_params
-    params.require(:component).permit(:name, :component_type, :order)
-  end
 end
->>>>>>> f97220cc4e7279c0bdf314f916161fc809db4ec2
