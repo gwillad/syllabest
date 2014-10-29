97d7d865e5e9664c4e37f784e73f1dbf3a742893
Paul Donahue <pdonahue@gemini.cs.hamilton.edu>
2014-10-29 15:57:06 -0400

    Added more validations and formatting for error display

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :syllabuses, dependent: :destroy
-  validates_presence_of :first_name
+  validates_presence_of :email, :password, :last_name, :first_name, :office, :school
+  validates_uniqueness_of :email
+  validates_length_of :password, minimum: 6, maximum: 30
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
