18ab3e6ed30cbc5f5f3f76a3211bc596aed1b7d2
Paul Donahue <pdonahue@gemini.cs.hamilton.edu>
2014-11-05 15:47:48 -0500

    Validation changes, currently errors with first name are the closest to final

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :syllabuses, dependent: :destroy
-  validates_presence_of :email, :password, :last_name, :first_name, :office, :school
-  validates_uniqueness_of :email
-  validates_length_of :password, minimum: 6, maximum: 30
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates_presence_of :email, :password, :last_name, :first_name, :office, :school, :message => "Field is required"
+  validates_uniqueness_of :email, :message => "A user with this e-mail already exists"
+  validates_length_of :password, minimum: 6, maximum: 30, :message => "Password must be between 6 and 30 characters."
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => "Invalid e-mail address"
 end
