572fb1eeff01d0ece53dbd4b121da088c7f6c7d0
Paul Donahue <pdonahue@gemini.cs.hamilton.edu>
2014-12-02 14:44:17 -0500

    Migration to remove password string and add passwor digest (attempt 2)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106200856) do
+ActiveRecord::Schema.define(version: 20141202194153) do
 
   create_table "components", force: true do |t|
     t.string   "component_type"
@@ -50,17 +50,18 @@ ActiveRecord::Schema.define(version: 20141106200856) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "border_class"
   end
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
     t.string   "last_name"
     t.string   "first_name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "office"
     t.string   "school"
+    t.string   "password_digest"
   end
 
 end
