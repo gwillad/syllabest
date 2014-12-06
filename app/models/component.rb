4fd20e3348507f4b22f8f59f56386ec2e24ff67c
Casey Collins <chcollin@gemini.cs.hamilton.edu>
2014-12-06 17:53:13 -0500

    Fix table updates as well

--- app/models/component.rb
+++ app/models/component.rb
@@ -2,7 +2,7 @@ class Component < ActiveRecord::Base
   belongs_to :syllabus
   has_one :plaintext, dependent: :destroy, autosave: true
   has_one :table, dependent: :destroy, autosave: true
-  attr_accessible :component_type, :id, :created_at, :updated_at, :syllabus_id, :order, :plaintext_attributes
+  attr_accessible :component_type, :id, :created_at, :updated_at, :syllabus_id, :order, :plaintext_attributes, :table_attributes
   accepts_nested_attributes_for :plaintext
   accepts_nested_attributes_for :table
 end
