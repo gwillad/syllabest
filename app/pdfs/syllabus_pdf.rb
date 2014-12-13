e2e7cab1402ac5de473e784627d8fe1b28bdf620
Adam Gwilliam <agwillia@hamilton.edu>
2014-12-13 18:06:22 -0500

    added pdf funcitonality

--- app/pdfs/syllabus_pdf.rb
+++ app/pdfs/syllabus_pdf.rb
@@ -14,58 +14,55 @@ class SyllabusPdf < Prawn::Document
         sTable(t)
       end
     end
-    
   end
-
+  
   def header
-#    font_size 20
     header_opts = @syllabus.header_options
+    puts header_opts
     if header_opts[0] == "1"
-      text @syllabus.title, size: 28, align: :center
+      text @syllabus.title, size: 24, align: :center
     end
     if header_opts[1] == "1"
-      text @syllabus.department + " " + @syllabus.course_num + " " +  @syllabus.course_type, size: 14, align: :center
+      text @syllabus.department + " " + @syllabus.course_num + " " +  @syllabus.course_type, size: 16, align: :center
     end
     if header_opts[2] == "1"
-      text @syllabus.school, size: 12, align: :center
+      text @user.school, size: 14, align: :center
+    end
     if header_opts[3] == "1"
-      text @syllabus.term, size: 12, align: :center
+      text @syllabus.term, size: 14, align: :center
     end
     if header_opts[4] == "1"
-      text "Instructor: " + @user.name, size: 12
+      text @syllabus.location, size: 14, align: :center
     end
-    #if header_opts[5] == "1"
-    #  text @syllabus.location, size: 12, align: :center
-    #end
-    #if header_opts[6] == "1"
-    #  text @syllabus.location, size: 12, align: :center
-    #end
-    #if header_opts[7] == "1"
-    #  text @syllabus.location, size: 12, align: :center
-    #end
-    #if header_opts[8] == "1"
-    #  text @syllabus.location, size: 12, align: :center
-    #end
-    #if header_opts[9] == "1"
-    #  text @syllabus.location, size: 12, align: :center
-    #end
+    if header_opts[5] == "1"
+      table [["Instructor:", @user.first_name + " " + @user.last_name]], cell_style: {borders: []}, width: 300
+    end
+    if header_opts[6] == "1"
+      table [[ "Office:", @user.office]], cell_style: {borders: []}, width: 300
+    end
+    if header_opts[7] == "1"
+      table [[ "Office Hours:", @syllabus.office_hrs.join(" ") ]], cell_style: {borders: []}, width: 300
+      #TODO!
+    end
+    if header_opts[8] == "1"
+      table [[ "Email:", @user.email]], cell_style: {borders: []}, width: 300
+    end
+    if header_opts[9] == "1"
+      table [[ "Phone:", @user.phone]], cell_style: {borders: []}, width: 300
+    end
     text "\n"
- #   font_size 10
   end
 
   def sTable(t)
     title = t[0]
-    text t.title, size: 15
-    puts t.contents
-    table t.contents, position: :center, header: true, width: 550
-    #t = t.slice(1..t.length)
+    text t.title, size: 14
+    table t.contents, position: :center, header: true, width: 550, cell_style: {borders: if t.border_class == "border_hidden" then [] else [:top, :right, :bottom, :left] end}
     text "\n\n"
   end
 
   def plain(p)
-    text p.title, size: 15
-    text "\n"
-    text p.contents
+    text p.title, size: 14
+    text p.contents, size: 11
     text "\n\n"
   end
 
